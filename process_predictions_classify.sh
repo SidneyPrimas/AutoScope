f00b29388095f1cc9fea53f7e30ded4eeee310f9
Sidney_Primas_2014_Personal_Computer <sidney.primas@gmail.com>
2018-02-01 20:55:54 -0500

    Fix an error in bash scripts used to process prediction folder

--- process_predictions_classify.sh
+++ process_predictions_classify.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 echo "Run Binary Segmentation + Classification Scripts"
 
-python urine_particles/process_urine_classify.py -r ./urine_particles/data/clinical_experiment/prediction_folder/sol1_rev1/
 python urine_particles/process_urine_segment.py -r ./urine_particles/data/clinical_experiment/prediction_folder/sol1_rev1/
+python urine_particles/process_urine_classify.py -r ./urine_particles/data/clinical_experiment/prediction_folder/sol1_rev1/
 
-python urine_particles/process_urine_classify.py -r ./urine_particles/data/clinical_experiment/prediction_folder/sol2_rev1/
 python urine_particles/process_urine_segment.py -r ./urine_particles/data/clinical_experiment/prediction_folder/sol2_rev1/
+python urine_particles/process_urine_classify.py -r ./urine_particles/data/clinical_experiment/prediction_folder/sol2_rev1/
 
-python urine_particles/process_urine_classify.py -r ./urine_particles/data/clinical_experiment/prediction_folder/sol3_rev1/
 python urine_particles/process_urine_segment.py -r ./urine_particles/data/clinical_experiment/prediction_folder/sol3_rev1/
+python urine_particles/process_urine_classify.py -r ./urine_particles/data/clinical_experiment/prediction_folder/sol3_rev1/
 
-python urine_particles/process_urine_classify.py -r ./urine_particles/data/clinical_experiment/prediction_folder/sol4_rev1/
 python urine_particles/process_urine_segment.py -r ./urine_particles/data/clinical_experiment/prediction_folder/sol4_rev1/
+python urine_particles/process_urine_classify.py -r ./urine_particles/data/clinical_experiment/prediction_folder/sol4_rev1/
 
-python urine_particles/process_urine_classify.py -r ./urine_particles/data/clinical_experiment/prediction_folder/sol5_rev1/
 python urine_particles/process_urine_segment.py -r ./urine_particles/data/clinical_experiment/prediction_folder/sol5_rev1/
+python urine_particles/process_urine_classify.py -r ./urine_particles/data/clinical_experiment/prediction_folder/sol5_rev1/
